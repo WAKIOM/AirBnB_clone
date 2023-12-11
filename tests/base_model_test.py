2923fe99dc1eb3d61780b07aa395f49c1a1f2e57
10-menachi <bobitlmr188221@spu.ac.ke>
2023-12-11 09:24:06 +0300

    Edit tests

--- tests/base_model_test.py
+++ tests/base_model_test.py
@@ -1,36 +1,41 @@
 import unittest
 from models.base_model import BaseModel
 
+
 class TestBaseModel(unittest.TestCase):
-   """
-   Test class for BaseModel
-   """
+    """
+    Test class for BaseModel
+    """
 
-   def setUp(self):
-       """
-       Set up method for the test class
-       """
-       self.base_model = BaseModel()
+    def setUp(self):
+        """
+        Set up method for the test class
+        """
+        self.base_model = BaseModel()
 
-   def test_str(self):
-       """
-       Test the __str__ method
-       """
-       self.assertIsInstance(str(self.base_model), str)
+    def tearDown(self) -> None:
+        return super().tearDown()
 
-   def test_save(self):
-       """
-       Test the save method
-       """
-       initial_updated_at = self.base_model.updated_at
-       self.base_model.save()
-       self.assertGreater(self.base_model.updated_at, initial_updated_at)
+    def str(self):
+        """
+        Test the __str__ method
+        """
+        self.assertIsInstance(str(self.base_model), str)
 
-   def test_to_dict(self):
-       """
-       Test the to_dict method
-       """
-       self.assertIsInstance(self.base_model.to_dict(), dict)
+    def save(self):
+        """
+        Test the save method
+        """
+        initial_updated_at = self.base_model.updated_at
+        self.base_model.save()
+        self.assertGreater(self.base_model.updated_at, initial_updated_at)
+
+    def to_dict(self):
+        """
+        Test the to_dict method
+        """
+        self.assertIsInstance(self.base_model.to_dict(), dict)
+
 
 if __name__ == '__main__':
-   unittest.main()
+    unittest.main()
