a43c24e2fd2bb89ce193696baefdeb7ff67f1d6d
WAKIOM <agnessmanga@gmail.com>
2023-12-11 09:48:10 +0300

    commit

--- console.py
+++ console.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""module defines and creates a console"""
+"""Module that defines and creates a console for the AirBnB project."""
+
 import cmd
+import ast
+import shlex
 from models import storage
 from models.user import User
 from models.state import State
@@ -8,117 +11,171 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
+    """
+    Command interpreter class for the AirBnB project.
 
-    prompt = "(hbnb) "
+    Attributes:
+    - prompt: The command prompt for the console.
+    - valid_classes: A list of valid class names in the AirBnB project.
+    """
+
+    prompt = '(hbnb) '
+    valid_classes = ['BaseModel', 'User', 'State', 'City',
+                     'Amenity', 'Place', 'Review']
 
     def emptyline(self):
-        """emptyline"""
+        """Do nothing on an empty line."""
         pass
 
     def do_quit(self, arg):
-        """Quit command to exit the program"""
+        """
+        Quit command to exit the program.
+
+        Usage:
+        $ quit
+        """
         return True
 
     def do_EOF(self, arg):
-        """Exit the program"""
+        """
+        Exit the program.
+
+        Usage:
+        $ EOF
+        """
         print()
         return True
 
     def do_create(self, arg):
         """
-        Creates a new instance of BaseModel,
-        saves it (to the JSON file) and prints the id
-        Usage: $ create BaseModel
-        If the class name is missing, print ** class name missing **
-        (ex: $ create)
-        If the class name doesn’t exist, print ** class doesn't exist **
-        (ex: $ create MyModel)
+        Creates a new instance of a specified class, saves it,
+        and prints the id.
+
+        Args:
+        - arg (str): The command-line argument containing the class name.
+
+        Usage:
+        $ create BaseModel
         """
-        if not arg:
+        args = shlex.split(arg)
+        if not args:
             print("** class name missing **")
             return
-
-        try:
-            new_instance = eval(arg)()
-            new_instance.save()
-            print(new_instance.id)
-        except NameError:
+        class_name = args[0]
+        if class_name not in self.valid_classes:
             print("** class doesn't exist **")
+            return
+        new_instance = globals()[class_name]()
+        new_instance.save()
+        print(new_instance.id)
 
     def do_show(self, arg):
         """
-        Prints the string repr of an instance based on class name and id
-        Usage: $ show BaseModel 1234-1234-1234
+        Prints the string representation of an instance.
+
+        Args:
+        - arg (str): The command-line argument containing the class name
+                     and instance id.
+
+        Usage:
+        $ show BaseModel 1234-1234-1234
         """
-        args = arg.split()
+        args = shlex.split(arg)
         if not args:
             print("** class name missing **")
             return
         class_name = args[0]
+        if class_name not in self.valid_classes:
+            print("** class doesn't exist **")
+            return
         if len(args) < 2:
             print("** instance id missing **")
             return
         instance_id = args[1]
         key = "{}.{}".format(class_name, instance_id)
         all_objs = storage.all()
-        if key in all_objs:
-            print(all_objs[key])
-        else:
+        if key not in all_objs:
             print("** no instance found **")
+            return
+        print(all_objs[key])
 
-    def do_destroy():
+    def do_destroy(self, arg):
         """
-         Deletes an instance based on the class name and id
-         (save the change into the JSON file)
-         Usage: $ destroy BaseModel 1234-1234-1234
+        Deletes an instance based on the class name and id.
+
+        Args:
+        - arg (str): The command-line argument containing the class name
+                     and instance id.
+
+        Usage:
+        $ destroy BaseModel 1234-1234-1234
         """
-        args = arg.split()
+        args = shlex.split(arg)
         if not args:
             print("** class name missing **")
             return
         class_name = args[0]
+        if class_name not in self.valid_classes:
+            print("** class doesn't exist **")
+            return
         if len(args) < 2:
             print("** instance id missing **")
             return
         instance_id = args[1]
         key = "{}.{}".format(class_name, instance_id)
         all_objs = storage.all()
-        if key in all_objs:
-            del all_objs[key]
-            storage.save()
-        else:
+        if key not in all_objs:
             print("** no instance found **")
+            return
+        del all_objs[key]
+        storage.save()
 
-    def do_all():
+    def do_all(self, arg):
         """
-        Prints all string representation of all instances based or
-        not on the class name
-        Usage: $ all BaseModel or $ all
+        Prints all string representation of instances.
+
+        Args:
+        - arg (str): The command-line argument containing the class name
+                     (optional).
+
+        Usage:
+        $ all BaseModel or $ all
         """
-        args = arg.split()
+        args = shlex.split(arg)
         all_objs = storage.all()
         if not args:
             print([str(obj) for obj in all_objs.values()])
-        elif args[0] in storage.valid_classes():
-            print([str(obj) for key, obj in all_objs.items()
-                  if args[0] == key.split('.')[0]])
         else:
-            print("** class doesn't exist **")
+            class_name = args[0]
+            if class_name not in self.valid_classes:
+                print("** class doesn't exist **")
+                return
+            filtered_objs = [str(obj) for obj in all_objs.values()
+                             if type(obj).__name__ == class_name]
+            print(filtered_objs)
 
-    def do_update():
+    def do_update(self, arg):
         """
-        Updates an instance based on the class name and id by adding or
-        updating attribute (save the change into the JSON file)
-        Usage: $ update BaseModel 1234-1234-1234 email "aibnb@mail.com"
+        Updates an instance based on the class name and id.
+
+        Args:
+        - arg (str): The command-line argument containing the class name,
+                    instance id, attribute name, and attribute value.
+
+        Usage:
+        $ update BaseModel 1234-1234-1234 email "aibnb@mail.com"
         """
-        args = arg.split()
+        args = shlex.split(arg)
         if not args:
             print("** class name missing **")
             return
         class_name = args[0]
+        if class_name not in self.valid_classes:
+            print("** class doesn't exist **")
+            return
         if len(args) < 2:
             print("** instance id missing **")
             return
@@ -135,13 +192,15 @@ class HBNBCommand(cmd.Cmd):
         if len(args) < 4:
             print("** value missing **")
             return
-        attribute_value = args[3]
-        instance = all_objs[key]
+        attribute_value_str = args[3]
         try:
-            setattr(instance, attribute_name, eval(attribute_value))
-        except ValueError:
+            attribute_value = ast.literal_eval(attribute_value_str) 
+        except (AttributeError, ValueError):
             print("** invalid attribute value **")
-        storage.save()
+            return
+        instance = all_objs[key]
+        setattr(instance, attribute_name, attribute_value)
+        instance.save()
 
 
 if __name__ == '__main__':
